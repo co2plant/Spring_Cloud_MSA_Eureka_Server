fa2f2f35956a4e1be8e9d85c00d64c5c0aa88ddb
co2plant <93631841+co2plant@users.noreply.github.com>
2025-04-04 22:00:47 +0900

    init:

---
+++ src/main/java/kr/co/inntavern/spring_cloud_msa_eureka_server/security/SecurityCustomConfig.java
@@ -0,0 +1,45 @@
+package kr.co.inntavern.spring_cloud_msa_eureka_server.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityCustomConfig {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf((auth)-> auth.disable());
+        httpSecurity
+                .authorizeHttpRequests((auth)-> auth.anyRequest().authenticated());
+        httpSecurity
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.builder()
+                .username("admin")
+                .password(bCryptPasswordEncoder().encode("1234"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1);
+    }
+}
---
+++ src/test/java/kr/co/inntavern/spring_cloud_msa_eureka_server/SpringCloudMsaEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.inntavern.spring_cloud_msa_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMsaEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
